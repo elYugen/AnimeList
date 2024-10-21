8471661327524c2d7eb31176308bfa3ea5e4f443
elbitoku <leo.langlois123@gmail.com>
2024-10-21 15:04:30 +0200

    added page for anime to see

--- vite.config.js
+++ vite.config.js
@@ -15,12 +15,12 @@ export default defineConfig({
         theme_color: '#1b1e26',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: 'favicon.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'favicon.png',
             sizes: '512x512',
             type: 'image/png',
           }
